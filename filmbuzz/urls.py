8074d52706e0bed8ff267a3753a41ed64670fac9
Ray Carter <raycarter158@gmail.com>
2024-12-02 16:49:59 +0000

    created home and about page

--- filmbuzz/urls.py
+++ filmbuzz/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import home, about
 
 urlpatterns = [
-    path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('about/', about, name='about'),
+    path('', home, name='home'),
 ]
