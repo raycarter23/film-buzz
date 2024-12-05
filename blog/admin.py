b6a9725bdd9915373226080791243e659cd5e39b
Ray Carter <raycarter158@gmail.com>
2024-12-05 14:34:20 +0000

    feat: create Comment model and register in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Post
+from .models import Post, Comment # Import both models
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
+    list_display = ('title', 'slug', 'created_at')  # Columns to display in the admin list view
+    search_fields = ('title')  # Add a search bar for these fields
+    list_filter = ('created_at',)  # Add filters for specific fields
+
+# Optional: Customise Comment admin display
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'date_added')  # Columns to display in the admin list view
+    search_fields = ('name', 'body')  # Add a search bar for these fields
+    list_filter = ('date_added',)  # Add filters for specific fields
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,19 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return '/%s/' % self.slug
+
+class Comment(models.Model):
+    """
+    Represents a comment made on a movie blog post
+    """
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
