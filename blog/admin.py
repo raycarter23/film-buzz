3bb9024cc1fe55c0a4cea1a41dcde7f61909d19d
Ray Carter <raycarter158@gmail.com>
2024-12-05 14:49:17 +0000

    heroku deployment commit

--- blog/admin.py
+++ blog/admin.py
@@ -5,13 +5,13 @@ from django.contrib import admin
 from .models import Post, Comment # Import both models
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('title',)}
-    list_display = ('title', 'slug', 'created_at')  # Columns to display in the admin list view
-    search_fields = ('title')  # Add a search bar for these fields
+    list_display = ['title', 'slug', 'created_at']  # Columns to display in the admin list view
+    search_fields = ['title']  # Add a search bar for these fields
     list_filter = ('created_at',)  # Add filters for specific fields
 
 # Optional: Customise Comment admin display
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('post', 'name', 'date_added')  # Columns to display in the admin list view
+    list_display = ['post', 'name', 'date_added']  # Columns to display in the admin list view
     search_fields = ('name', 'body')  # Add a search bar for these fields
     list_filter = ('date_added',)  # Add filters for specific fields
 
