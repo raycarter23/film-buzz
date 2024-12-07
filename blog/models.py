5efbcd6f8d3171b73eaaf983f801f727eded58b4
Ray Carter <raycarter158@gmail.com>
2024-12-07 15:38:35 +0000

    fix: Category model migration error

--- blog/models.py
+++ blog/models.py
@@ -2,41 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Post(models.Model):
-    """
-    Represents movie blog posts
-    """
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(default='')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.CharField(max_length=20)
-    image = models.FileField(upload_to='media/blog')
-
-    def __str__(self):
-        return self.title
-    
-    def get_absolute_url(self):
-        return '/%s/' % self.slug
-
-class Comment(models.Model):
-    """
-    Represents a comment made on a movie blog post
-    """
-    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
-    comment = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
-    class Meta:
-        verbose_name = "Comment"
-        verbose_name_plural = "Comments"
-
-    def __str__(self):
-        return f'Comment from {self.user} on {self.post}'
-
 class Category(models.Model):
     """
     Represents a movie category
@@ -61,4 +26,39 @@ class Category(models.Model):
     
     def get_absolute_url(self):
         return '/%s/' % self.slug
+
+class Post(models.Model):
+    """
+    Represents movie blog posts
+    """
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(default='')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    image = models.FileField(upload_to='media/blog')
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return '/%s/' % self.slug
+
+class Comment(models.Model):
+    """
+    Represents a comment made on a movie blog post
+    """
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+
+    def __str__(self):
+        return f'Comment from {self.user} on {self.post}'
+
 
