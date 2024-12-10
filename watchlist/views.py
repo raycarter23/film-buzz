f004696ea1518b30b5a7439f9feaeaf8cf2aa2f6
Ray Carter <raycarter158@gmail.com>
2024-12-10 12:59:21 +0000

    feat: search movies and view watchlist views

--- watchlist/views.py
+++ watchlist/views.py
@@ -34,3 +34,29 @@ def add_to_watchlist(request, movie_id):
         watchlist.save()
 
     return redirect('watchlist')
+
+@login_required
+def search_movies(request):
+    query = request.GET.get('query')
+    tmdb_search_url = f"https://api.moviedb.org/3/search/movie?api_key={settings.TMDB_API_KEY}&query={query}"
+    response = request.get(tmdb_search_url)
+    movies = response.json().get('results', []) if response.status_code == 200 else []
+
+    return render(request, 'watchlist/search_results.html', {'movies': movies})
+
+@login_required
+def view_watchlist(request):
+    watchlist = Watchlist.objects.filter(user=request.user)
+    for entry in watchlist:
+        tmdb_movie_url = f"https://api.themoviedb.org/3/movie/{entry.movie.tmdb_id}?api_key={settings.TMDB_API_KEY}"
+        response = request.get(tmdb_movie_url) 
+
+        if response.status_code == 200:
+            data = response.json()
+            entry.movie.poster = f"https://image.tmdb.org/t/p/w500{data['poster_path']}"
+            entry.movie.title = data['title']
+            entry.movie.release_date = data['release_date']
+
+    return render(request, 'watchlist/watchlist.html', {'watchlist': watchlist})
+
+
