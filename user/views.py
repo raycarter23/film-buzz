aac9e8ff6f9d1524453206b3bd81aa6dd3c8ad6f
Ray Carter <raycarter158@gmail.com>
2024-12-12 13:47:50 +0000

    feat: created views for user profile

--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
+
+@login_required
+def view_profile(request):
+    profile = UserProfile.objects.get_or_create(user=request.user)
+
+    return render(request,'user/profile.html', {'profile': profile})
+
+@login_required
+def edit_profile(request):
+    profile = UserProfile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=profile)
+
+        if form.is_valid():
+            form.save() 
+            return redirect('profile')
+    
+    else:
+        form = UserProfileForm(instance=profile)
+    return render(request, 'user/edit_profile.html', {'form': form})
+
