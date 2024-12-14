f7d6e58cce144b496a4a12c894c2e189045adeb3
Ray Carter <raycarter158@gmail.com>
2024-12-14 19:51:32 +0000

    feat: auto slug generation for new posts based on title

--- blog/forms.py
+++ blog/forms.py
@@ -9,5 +9,5 @@ class CommentForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'slug', 'content', 'category', 'image')
+        fields = ('title', 'content', 'category', 'image')
     
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 class Category(models.Model):
@@ -39,6 +40,11 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
     image = models.FileField(upload_to='media/blog')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
