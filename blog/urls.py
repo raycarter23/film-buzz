73157f48a3a78d1c7e43f350da23d1c641f3eb78
Ray Carter <raycarter158@gmail.com>
2024-12-15 16:38:29 +0000

    feat: add urls for edit and delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include 
 from . import views
-from user.views import create_post
+from user.views import create_post, edit_post, delete_post
 
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('search/', views.search, name='search'),
     path('create_post/', create_post, name='create_post'),
+    path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
+    path('delete_post/<int:post_id>/', delete_post, name='delete_post'),
     path('<slug:category_slug>/<slug:slug>/', views.details, name='details'),
     path('<slug:category_slug>/<slug:slug>/comment/', views.comment, name='comment'),
     path('<slug:category_slug>/<slug:slug>/delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
